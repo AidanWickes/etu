c63c4a47b28fc8be5a42cd510c2c47a6acd79ce1
Ella Chambers <33899496+elloella@users.noreply.github.com>
2020-11-16 16:14:09 +0000

    Donut Chart

--- lib/screens/timer/timer.dart
+++ lib/screens/timer/timer.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:wellbeing_app/controllers/donut_chart.dart';
 
 class Timer extends StatefulWidget {
   @override
   _TimerState createState() => _TimerState();
 }
-class _TimerState extends State<Timer> {
+    class _TimerState extends State<Timer> {
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  Text('Tracking Page'),
+      body: Container(
+        child: SizedBox(
+          height: 300,
+          child: DonutPieChart.withSampleData(),
+        ),
+      ),
       
     );
   }
 }
+
